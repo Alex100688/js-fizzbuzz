2a918a3405cb75dabaa3ed7ad153ababd2f48641
Alex100688 <alessiozingaro2@gmail.com>
2022-10-24 17:55:04 +0200

    Aggiunge il back-color al container e un titolo h1

--- js/script.js
+++ js/script.js
@@ -5,10 +5,7 @@ for(let i = 1; i <= 100; i++){
     boxNumber.classList.add(`box_number`);
     boxNumber.innerHTML = i;
     fizzBox.append(boxNumber);
-    if( i % 3===0){
-        boxNumber.innerHTML = (`fizz`)}else if( i % 5===0){
-            boxNumber.innerHTML=(`buzz`)}
-           
+    if( i % 3===0){ boxNumber.innerHTML = (`fizz`)}else if( i % 5===0){boxNumber.innerHTML=(`buzz`)}       
     }
                
 
