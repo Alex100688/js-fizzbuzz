179a39ed6e118bb8913b3d45c03122ae695a746d
Alex100688 <alessiozingaro2@gmail.com>
2022-10-24 16:59:47 +0200

    Agggiuge la regola del modulo in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 "use strict";
 const fizzBox=document.querySelector(".box");
-for(let i = 1; i <= 100; i++){
+for(let i = 1; i <= 100; i++)
+{   if(i % 3===0){
+    document.querySelector(`.box_number`).innerHTML=(`fizz`);
+}
     const boxNumber=document.createElement(`div`);
     boxNumber.classList.add(`box_number`);
     boxNumber.innerHTML = i;
     fizzBox.append(boxNumber);
 }
-
