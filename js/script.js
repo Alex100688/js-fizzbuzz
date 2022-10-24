229a1e2205ffbc53412bddb55162b0ab510b107e
Alex100688 <alessiozingaro2@gmail.com>
2022-10-24 17:43:09 +0200

    Aggiunge i multipli di 3  e di 5 modulo nel js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 "use strict";
 const fizzBox=document.querySelector(".box");
-for(let i = 1; i <= 100; i++)
-{   if(i % 3===0){
-    document.querySelector(`.box_number`).innerHTML=(`fizz`);
-}
+for(let i = 1; i <= 100; i++){
     const boxNumber=document.createElement(`div`);
     boxNumber.classList.add(`box_number`);
     boxNumber.innerHTML = i;
     fizzBox.append(boxNumber);
-}
+    if( i % 3===0){
+        boxNumber.innerHTML = (`fizz`)}else if( i % 5===0){
+            boxNumber.innerHTML=(`buzz`)}
+           
+    }
+               
+
